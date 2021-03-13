0caaa2ac6c7c4091633dfc7d352abb76d8764328
Nu Uyen Thi PHAN <thiphan94@gmail.com>
2021-03-13 12:03:25 +0100

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {FirebaseService} from './services/firebase.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'angular-app-todo-PHAN';
+  isSignedIn = false
+  constructor(public FirebaseService :  FirebaseService){}
+  ngOnInit(){
+    if(localStorage.getItem('user')!==null)
+    this.isSignedIn =true
+    else
+    this.isSignedIn =false
+  }
+  async onSignup(email1:string, password1:string){
+    await this.FirebaseService.signup(email1, password1)
+    if(this.FirebaseService.isLoggedIn)
+    this.isSignedIn = true
+  }
+  async onSignin(email:string, password:string){
+    await this.FirebaseService.signup(email, password)
+    if(this.FirebaseService.isLoggedIn)
+    this.isSignedIn = true
+  }
+  handleLogout(){
+    this.isSignedIn = false
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { FirebaseService } from '../services/firebase.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  @Output() isLogout = new EventEmitter<void>()
+  constructor(public firebaseService : FirebaseService) { }
 
   ngOnInit(): void {
+  }
+  logout(){
+    this.firebaseService.logout()
+    this.isLogout.emit()
   }
 
 }
